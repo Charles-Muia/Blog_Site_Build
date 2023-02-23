9fd1b02558c49d94d2a3bfcf468e69f2acfbc577
Charles-Muia <Charles.K.Muia@gmail.com>
2023-02-23 16:55:51 +0300

    Responsive_Blog_site_Commit

--- index.php
+++ index.php
@@ -281,9 +281,12 @@
 <!---->
 <!--                scrollTopButton.addEventListener('click', ()=> {-->
 <!--                    window.scrollTo(0, 0);-->
+
+
 <!---->
 <!--                })-->
 <!--            </script>-->
+
 
 
             <script>
@@ -292,6 +295,7 @@
 
                 // When the user scrolls down 20px from the top of the document, show the button
                 window.onscroll = function() {scrollFunction()};
+
 
                 function scrollFunction() {
                     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200 ) {
